104f5cdf0cb293431fecda04e95fcd7d51b0d10e
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-05-18 11:07:28 -0300

    Add NavLink

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
-import React, { PureComponent } from 'react'
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import React, { PureComponent } from "react";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
 
-import './css/style.css'
-import { Li } from 'glamorous';
+import "./css/style.css";
+import { Li } from "glamorous";
 
 class App extends PureComponent {
   render() {
@@ -12,65 +12,73 @@ class App extends PureComponent {
       <BrowserRouter>
         <div>
           <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/sobre'>Sobre</Link></li>
-            <li><Link to='contato'>Contato</Link></li>
-            <li><Link to='/blog'>Blog</Link></li>
+            <li>
+              <NavLink to="/" exact>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active-link" to="/sobre">
+                Sobre
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={{ color: "red" }} to="contato">
+                Contato
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/blog">Blog</NavLink>
+            </li>
           </ul>
 
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/(sobre|contato)' component={Page} />
-            <Route path='/blog' component={Blog} />
+            <Route exact path="/" component={Home} />
+            <Route path="/(sobre|contato)" component={Page} />
+            <Route path="/blog" component={Blog} />
             <Route component={Error404} />
           </Switch>
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-const Error404 = () => (
-  <h1>Página não econtrada</h1>
-)
+const Error404 = () => <h1>Página não econtrada</h1>;
 
-const Home = () => (
-  <h1>Home</h1>
-)
+const Home = () => <h1>Home</h1>;
 
-const Page = ({ match }) => (
-  <h1>{match.url}</h1>
-)
+const Page = ({ match }) => <h1>{match.url}</h1>;
 
 const Blog = () => (
   <div>
     <h1>Blog</h1>
 
     <ul>
-      <li><Link to='/blog/post-1'>Post 1</Link></li>
-      <li><Link to='/blog/post-2'>Post 2</Link></li>
+      <li>
+        <NavLink to="/blog/post-1">Post 1</NavLink>
+      </li>
+      <li>
+        <NavLink to="/blog/post-2">Post 2</NavLink>
+      </li>
     </ul>
 
     <Switch>
-      <Route exact path='/blog' component={NoPosts} />
-      <Route path='/blog/:post(post-[12])' component={Post} />
+      <Route exact path="/blog" component={NoPosts} />
+      <Route path="/blog/:post(post-[12])" component={Post} />
       <Route component={Post404} />
     </Switch>
   </div>
-)
+);
 
-const Post404 = () => (
-  <h1>Esse post não existe</h1>
-)
+const Post404 = () => <h1>Esse post não existe</h1>;
 
 const Post = ({ match }) => (
   <div>
     <h2>Post: {match.params.post}</h2>
   </div>
-)
+);
 
-const NoPosts = () => (
-  <p>Selecione um post</p>
-)
+const NoPosts = () => <p>Selecione um post</p>;
 
-export default App
+export default App;
